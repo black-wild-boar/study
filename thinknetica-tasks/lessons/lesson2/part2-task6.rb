1e14dbc9ded1374b79e9ceed47b3821489900dd0
mihailkovalevskiy <mihailkovalevskiy@gmail.com>
2016-11-15 16:28:54 +0700

    Lesson 2

---
+++ thinknetica-tasks/lessons/lesson2/part2-task6.rb
@@ -0,0 +1,56 @@
+#Bazar
+
+stop_word = "стоп"
+
+hash_bazar = {}
+
+goods = ""
+
+#ввод данных
+until goods == stop_word do
+
+  puts "Введите наименование товара (для останова введите слово ""стоп"")."
+  goods = gets.chomp
+  break if goods == stop_word
+
+  puts "Цена за единицу"
+  price_per_one = gets.chomp.to_f
+  if price_per_one <= 0 
+    puts "Цена за единицу должна быть больше 0."
+    break
+  end
+
+    puts "Количество товара"
+    count = gets.chomp.to_f
+  if count <= 0 
+    puts "Количество должно быть больше 0."
+    break
+  end
+
+  hash_bazar[goods.to_sym] = {price: price_per_one, count: count}
+
+end
+
+hash_counts = hash_bazar.size
+
+array_hash_keys = hash_bazar.keys.to_a
+
+sum_price = sum_all_goods = 0.to_f
+
+#выборка и суммирование значений ключа price по каждому хэшу базара
+for i in 0..array_hash_keys.size-1
+
+  goods = array_hash_keys[i]
+  
+#получаю значение ключа goods 
+  goods_value = hash_bazar[array_hash_keys[i]]
+  
+  sum_price = goods_value[:price] * goods_value[:count]
+  
+  puts "Товар #{goods}. Сумма по товару #{sum_price.round 2}."
+
+  sum_all_goods = sum_all_goods + sum_price
+  
+end
+
+puts "Сумма покупок = #{sum_all_goods.round 2}"
--- tmp-code.rb
+++ tmp-code.rb
@@ -8,3 +8,129 @@ strings.each do |s|
 end
 
   puts new_arr[1].class
+
+
+  #====
+#  part2-task6.rb 
+
+stop_word = "stop"
+arr_rinok = []
+#hash_rinok = Hash.new { |tovar, price| hash[price] = Hash.new(0) }
+#hash_rinok = { 
+#              tovar_title: ("").to_sym,
+#              tovar_price: Hash.new(0)
+#              }
+
+hash_rinok = {}
+
+tovar = ""
+
+until tovar == stop_word do
+  puts "Товар (для останова введите слово stop)"
+  tovar = gets.chomp
+  break if tovar == stop_word
+
+  puts "Цена за единицу"
+  price_per_one = gets.chomp
+  puts "Количество товара"
+  count = gets.chomp
+
+  #puts tovar.to_sym
+  #puts price.to_f
+  #puts count.to_f
+
+#arr_rinok << {tovar_title: tovar.to_sym, tovar_price: [price.to_f, count.to_f]}
+#hash_rinok[tovar_title:, tovar_price:] << {tovar.to_sym, (price: price.to_f, count: count.to_f)}
+
+#arr_rinok << {tovar: tovar.to_sym, price: price.to_f, count: count.to_f}
+#hash_rinok[:tovar] = tovar.to_sym
+#hash_rinok[:price] = price.to_f
+#hash_rinok[:count] = count.to_f
+#hash_rinok.push[tovar.to_sym, price.to_f, count.to_f]
+
+#перезаписывает т.к. одинаковый ключ
+#hash_rinok =  {tovar: tovar.to_sym, price: {price: price.to_f, count: count.to_f}}
+puts hash_rinok
+
+hash_rinok[tovar.to_sym] = {price: price_per_one.to_f, count: count.to_f}
+#hash_rinok[price] = {price: price.to_f, count: count.to_f}
+
+
+puts "hash_rinok #{hash_rinok}"
+
+#puts "hash_rinok #{hash_rinok}"
+#puts "hash2_rinok #{hash2_rinok}"
+
+#puts hash_rinok
+
+#hash_rinok[:tovar] = tovar.to_sym 
+#hash_rinok[:price] = {price: price.to_f, count: count.to_f}
+
+#hash_rinok.store(tovar: tovar.to_sym, price: {price: price.to_f, count: count.to_f})
+
+#hash_rinok[:tovar] = tovar.to_sym
+#hash_rinok[:tovar][:price] = [price, count)
+
+
+end
+
+#puts hash_rinok.keys
+#puts hash_rinok.values
+
+#t={}
+#t = {a: "a1", b: {b1: 2, b2: 3}}
+#puts t[:b][:b1] + t[:b][:b2]
+
+#puts hash_rinok[:price].price_per_one + hash_rinok[:price].count
+
+price_summa = 0
+hash_counts = hash_rinok.size
+puts hash_counts
+
+#for i in 1..hash_counts
+#puts hash_rinok[:tovar]
+#end
+
+arr_hash_keys = hash_rinok.keys.to_a
+
+puts "arr_hash_keys #{arr_hash_keys}"
+puts "arr_hash_keys.size #{arr_hash_keys.size}"
+
+
+sum = 0.to_f
+for i in 0..arr_hash_keys.size-1
+
+tovar = arr_hash_keys[i]
+k = hash_rinok[arr_hash_keys[i]]
+#sum=k[:price]+k[:value]
+puts k[:price]
+puts k[:count]
+sum = k[:price] + k[:count]
+puts "tovar #{tovar} k #{k} sum #{sum}"
+end
+#hash_rinok.each { |key| 
+#hash_rinok.each_value { |value| print "v #{value}" }
+# puts "k #{key}" } 
+
+
+#  price_summa = hash_rinok[:price][:price_per_one] * hash_rinok[:price][:count]
+#  print "h #{tovar} #{price} #{price_summa}" 
+
+#puts hash_rinok[]
+#puts hash_rinok[:a1][:price]
+#puts hash_rinok[:price]
+#end
+
+#hash_rinok.each do
+#  puts "Покупка товара #{hash_rinok(1).to_s} на сумму 5" 
+#end
+
+
+#arr_rinok.each {|tovar_title, tovar_price| puts "#{tovar_title} #{tovar_price} сумма за товар " + (5+7).to_s }
+
+#puts arr_rinok
+#puts hash_rinok
+
+#arr_rinok.each {|tovar,price,count| print "a #{tovar} #{price} #{count}" }
+#hash_rinok.each {|tovar,price,count| print "h #{tovar} #{price} #{count}" }
+
