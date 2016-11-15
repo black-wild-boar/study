e9276d794ca94e1c592b85c1c95286bd38a2729a
mihailkovalevskiy <mihailkovalevskiy@gmail.com>
2016-11-15 17:57:26 +0700

    Lesson 2. Task 5. Code fix.

--- thinknetica-tasks/lessons/lesson2/part2-task5.rb
+++ thinknetica-tasks/lessons/lesson2/part2-task5.rb
@@ -12,36 +12,24 @@ year = gets.chomp.to_i
 
 array_of_months = [31,28,31,30,31,30,31,31,30,31,30,31]
 
-if day <= 0 || day > 30 || month <= 0 || month > 12 || year <=0
-  puts "Number of day can be only 1 .. 31"
-  puts "Number of month can be only 1 .. 12"
-  puts "Number of year can be only >= 1"
 #test for leap-year
+if (year % 4 == 0) || (year % 100 == 0) || (year == 2000)
+  array_of_months[1] += 1
+end
+
+if day <= 0 || day > 30 || month <= 0 || month > 12 || year <=0 || day>array_of_months[month-1]
+  puts "Number of day can be only 1 .. 31 or number of month can be only 1 .. 12 or number of year can be only >= 1 or incorrect day count at month #{day}.#{month}.#{year}"
 else
-  if (year % 4 == 0) || (year % 100 == 0) || (year == 2000)
-    array_of_months[month-1] += 1
-    if day>array_of_months[month-1]
-      puts "Incorrect day count at month #{day}.#{month}.#{year}"
-    else
-      puts "Correct day count at month #{day}.#{month}.#{year}. Leap-year."
-      day_position = really_day_position = 0
-      for i in 1..month-1 do 
-        day_position = day_position + array_of_months[i]
-      end
-      really_day_position = day_position + day + year
-      puts "Really_day_position = #{really_day_position}"
-    end
-  else
-    if day>array_of_months[month-1]
-      puts "Incorrect day count at month #{day}.#{month}.#{year}"
-    else
-      puts "Correct day count at month #{day}.#{month}.#{year}. Not leap-year."
-      day_position = really_day_position = 0
-      for i in 1..month-1 do 
-        day_position = day_position + array_of_months[i]
-      end
-      really_day_position = day_position + day + year
-      puts "Really_day_position = #{really_day_position}"
-    end
+
+  day_position = really_day_position = 0
+
+  for i in 1..month-1 do 
+    day_position += array_of_months[i-1]
   end
+
+puts "Data #{day}.#{month}.#{year}"
+
+really_day_position = day_position + day + year
+puts "Really_day_position = #{really_day_position}"
+
 end
