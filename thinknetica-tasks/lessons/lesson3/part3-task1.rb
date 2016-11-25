e73e698db92ed5cf72fc454314822891cc73054f
black-wild-boar <mihailkovalevskiy@gmail.com>
2016-11-25 21:25:46 +0700

    Lesson 3. Task1.

--- thinknetica-tasks/lessons/lesson3/part3-task1.rb
+++ thinknetica-tasks/lessons/lesson3/part3-task1.rb
@@ -6,42 +6,16 @@ class Station
   @@passanger_count = 0
   @@cargo_count = 0
 
-  def initialize(station_name)#, train_type)
-#train_type: 0 passanger's, 1 - cargo
+  def initialize(station_name)
     @station_name = station_name
-#    @train_type = train_type
     @@trains[station_name] = {}
-#    @@passanger_count += 1 if @train_type == 0
-#    @@cargo_count += 1 if @train_type == 1
-#    if @train_type = 0 
-#      @@passanger_count += 1
-#      elsif @train_type = 1
-#        @@cargo_count += 1
-#      else
-#        puts "Train type must be: 0  - passanger's, 1 - cargo"
-#      end
-#    end
-
-#    @list_stations = {}
   end
 
   def add_train(train_number, train_type)
-#    puts "Перечень станций: #{@@trains}"
-#    puts "Введите наименование станции"
-#    name_station = gets.chomp
-#    @@trains
-#    @@trains.each
     @train_number = train_number
     @train_type = train_type
     
     @@trains[@station_name][train_number] = train_type
-
-#    puts "Введите имя станции, после которой добавить"
-#    name_previous = gets.chomp
-#    @@trains[name_station] = 0
-#    puts "Добавлена станция #{name_station} после станции #{name_previous}"
-#    @list_stations()
-#    @train_type = train_type
   end
 
   def Station.all_trains
@@ -51,21 +25,10 @@ class Station
  def remove_train(train_number)
     @train_number = train_number
     @@trains[@station_name].delete(train_number)
-
-
-#   train_number = gets.chomp
-#    @@trains.each_with_index | value, index| do
-#      if @@trains.include(train_number)? @@trains[index].delete
-#      end
-#   @@trains.foreach do |index|
-#     delete.@@trains if  == i
   end
       
-# end
-#   delete
-# end
-
   def show_train
+    @station_name
     puts "It is #{@station_name} train. It #{@train_type} type"
   end
 
@@ -76,28 +39,24 @@ class Station
   def Station.passanger_count
     @@passanger_count
   end
-
 end
 
-
 #2
-class Route
+class Route < Station
 
   @@route_list = Array.new(0)
   
-  def initialize(first_station, last_station)
+  def initialize(station_name, first_station, last_station)
+    @station_name = station_name
+
     @first_station = first_station
     @last_station = last_station
     @@route_list[0] = first_station
     @@route_list[1] = last_station
-   super
+   super(station_name)
    end
 
   def add_station(station_name)
-    #tmp = @@route_list[-1]
-    #@@route_list[-1] = station_name
-    #@@route_list.push(tmp)
-    #insert(<position>,<value1>,<value2>),<value3>))
     @@route_list.insert(@@route_list.size-1, station_name)
   end
 
@@ -113,26 +72,23 @@ end
 
 #3
 class Train < Route
-attr_accessor :train_number, :train_type, :carriage_count, :speed
-@speed = 0
+#attr_accessor :train_number, :train_type, :carriage_count, :speed
+  def initialize(station_name, first_station, last_station, train_number, train_type, carriage_count, speed = 0)
+    @station_name = station_name
 
-#    @train_number = train_number
-#    @train_type = train_type
-#    @carriage_count = carriage_count
-#    @speed = speed
+    @first_station = first_station
+    @last_station = last_station
 
+    @train_number = train_number
+    @train_type = train_type
+    @carriage_count = carriage_count
+    @speed = speed
+    super(station_name, first_station, last_station)
+  end
 
-#  def initialize(train_number, train_type, carriage_count, speed = 0)
-#    @train_number = train_number
-#    @train_type = train_type
-#    @carriage_count = carriage_count
-#    @speed = speed
-#    add_station
-#  end
-
-# def increase_speed(speed)
-#   @speed = speed
-# end
+ def increase_speed(speed)
+   @speed = speed
+ end
 
   def show_speed
     puts "Current speed = #{@speed}"
@@ -142,9 +98,9 @@ attr_accessor :train_number, :train_type, :carriage_count, :speed
     @speed = 0
   end
 
-# def carriage_count
-#  @carriage_count
-# end
+ def carriage_count
+  @carriage_count
+ end
 
   def change_carriage_count(count)
     #+1 -1
@@ -159,5 +115,4 @@ attr_accessor :train_number, :train_type, :carriage_count, :speed
       puts "Train is moving with speed = #{@speed} or insert correct value(1 or -1). count = #{@count}"
     end
   end
-
 end
---
+++ tmp-code0.rb
@@ -0,0 +1,8 @@
+def create_record(attributes, rise_error = false)
+  record = build_record(attributes)
+  yield(record) if block_given?
+  saved = record.save
+  set_new_record(record)
+  raise RecordInvalid.new(record) if !saved && rise_error
+  record
+end
