d1c504110f55a1b8bba5d2b009ac68c312ef54f7
mihailkovalevskiy <mihailkovalevskiy@gmail.com>
2016-11-30 16:50:54 +0700

    Lesson3. Task 1. Fix1.

--- thinknetica-tasks/lessons/lesson3/part3-task1.rb
+++ thinknetica-tasks/lessons/lesson3/part3-task1.rb
@@ -1,118 +1,163 @@
 #Management of railway station
 
-#1
-class Station 
+#class Route
+#  attr_reader :stations
+#end
+
+class Train
+
+  attr_accessor :route
   @@trains = {}
-  @@passanger_count = 0
-  @@cargo_count = 0
 
-  def initialize(station_name)
-    @station_name = station_name
-    @@trains[station_name] = {}
-  end
+#возможная схема
+#{number: [[train_type, carriage_count, current_stations],{route: stations}]}
 
-  def add_train(train_number, train_type)
-    @train_number = train_number
-    @train_type = train_type
-    
-    @@trains[@station_name][train_number] = train_type
-  end
-
-  def Station.all_trains
-    @@trains
-  end
-
- def remove_train(train_number)
-    @train_number = train_number
-    @@trains[@station_name].delete(train_number)
-  end
-      
-  def show_train
-    @station_name
-    puts "It is #{@station_name} train. It #{@train_type} type"
-  end
-
-  def Station.cargo_count
-    @@cargo_count
-  end
-
-  def Station.passanger_count
-    @@passanger_count
-  end
-end
-
-#2
-class Route < Station
-
-  @@route_list = Array.new(0)
-  
-  def initialize(station_name, first_station, last_station)
-    @station_name = station_name
-
-    @first_station = first_station
-    @last_station = last_station
-    @@route_list[0] = first_station
-    @@route_list[1] = last_station
-   super(station_name)
-   end
-
-  def add_station(station_name)
-    @@route_list.insert(@@route_list.size-1, station_name)
-  end
-
-  def self.delete_station(station_name)
-    @@route_list.delete(station_name)
-  end
-
-  def list_stations
-    @@route_list
-  end
-
-end
-
-#3
-class Train < Route
-#attr_accessor :train_number, :train_type, :carriage_count, :speed
-  def initialize(station_name, first_station, last_station, train_number, train_type, carriage_count, speed = 0)
-    @station_name = station_name
-
-    @first_station = first_station
-    @last_station = last_station
-
+  def initialize(train_number, train_type, carriage_count)
     @train_number = train_number
     @train_type = train_type
     @carriage_count = carriage_count
-    @speed = speed
-    super(station_name, first_station, last_station)
-  end
-
- def increase_speed(speed)
-   @speed = speed
- end
-
-  def show_speed
-    puts "Current speed = #{@speed}"
-  end
-
-  def stop
+    @current_station = 0
+    @@trains[@train_number] = [@train_type, carriage_count] 
+    #@@trains[@train_number] = [@train_type, @carriage_count, @current_station, route] 
+#???  как получить значение - имя создаваемого объекта класса !!! через метод-геттер описаный в классе объекта
+#тогда будет ключ = имя, значение = массив
     @speed = 0
   end
 
- def carriage_count
-  @carriage_count
- end
+  def current_station
+    route.stations[@current_station]
+    puts "Текущая станция #{route.stations[@current_station].show_station} "
+    eee = route.stations[@current_station].show_station
+  #  puts $trains_at_stations
+    $trains_at_stations[eee.to_sym].push(@train_number)
 
-  def change_carriage_count(count)
-    #+1 -1
-    puts "1 to increase carriage_count."
-    puts "-1 to decrease carriage_count."
-    @count = count
+#наверное должен быть хэш {station1: [t1,t2], station2: [t3,t4],...}
+  end
 
-    if @speed == 0 && (@count == 1 || @count == -1) && @carriage_count > 0
-      @carriage_count += 1 if @count == 1
-      @carriage_count -= 1 if @count == -1
+  def next_station
+    if not route.stations[@current_station+1].nil?
+      #??? как вывести название станции !!! через метод-геттер описаный в классе объекта
+      #puts route.at(route.stations[@current_station)
+      #print @current_station
+      @current_station += 1
     else
-      puts "Train is moving with speed = #{@speed} or insert correct value(1 or -1). count = #{@count}"
+      puts "It's last station" #{@stations}"
+      puts @current_station
     end
   end
+
+  def prev_station
+    if @current_station >= 1 
+#      puts @current_station
+      @current_station -= 1
+    else
+      puts "It's first station"
+ #     puts @current_station
+    end
+  end
+
+  def show_next_station
+    route.stations[@current_station+1] if not route.stations[@current_station+1].nil?
+  end
+
+  def show_prev_station
+    route.stations[@current_station-1] if @current_station >= 1
+  end
+
+  def self.show_trains
+    @@trains
+  end
+
+  def speed_up(count)
+    @speed += count if count > 0
+    puts "Текущая скорость: #{@speed}"
+  end
+
+  def speed_down(count)
+    @speed -= count if @speed > 0 && count > 0
+    puts "Текущая скорость: #{@speed}"
+  end
+
+  def current_speed
+    puts "Текущая скорость: #{@speed}"
+  end
+
+  def carriage_count_up(count)
+    @carriage_count += count if @speed == 0 && count > 0
+  end
+
+  def carriage_count_down(count)
+    @carriage_count -= count if @speed == 0 && (@carriage_count-count)<0 && count > 0
+  end
+
+  def current_carriage_count
+    @carriage_count
+  end
+
 end
+
+class Station
+  #attr_accessor :current_station
+  $trains_at_stations = {}
+  #{station: [t1,t2,...]}
+
+  def initialize(station)
+    @station = station
+    $trains_at_stations[@station] = []
+#    @@all_routes[@first_station] = []
+#    @@all_routes[@last_station] = []
+  end
+
+  def self.show_stations
+    $trains_at_stations
+  end
+
+  def go_train(train)
+    @train = train
+    $trains_at_stations.delete(@train)
+  end
+
+  def show_station
+    @station
+  end
+
+end
+
+class Route
+
+  attr_reader :stations
+  @@all_routes = {}
+
+  def initialize(first_station , last_station)
+    @first_station = first_station
+    @last_station = last_station
+    @stations = [@first_station,@last_station]
+    @@all_routes[@stations] = []
+  end
+
+
+  def add_station(station)
+    @station = station
+    @stations.insert(@stations.size-1,@station)
+  end
+
+  def del_station(station)
+    @station = station
+    @stations.delete(@station)
+  end
+
+  def self.show_routes
+    @@all_routes
+  end
+
+end
+
+#s1 = Station.new("nsk")
+#s2 = Station.new("msk")
+#s3 = Station.new("spb")
+
+#r1 = Route.new(s1,s2)
+#r2 = Route.new(s2,s3)
+
+#t1 = Train.new(101,1,5)
+#t2 = Train.new(201,2,7)
--- tmp-code-3.rb
+++ tmp-code-3.rb
@@ -23,8 +23,12 @@ class Train
 
   def current_station
     route.stations[@current_station]
-    $trains_at_stations[route.stations[@current_station]] = @train_number
-#должен быть хэш {station1: [t1,t2], station2: [t3,t4],...}
+    puts "Текущая станция #{route.stations[@current_station].show_station} "
+    eee = route.stations[@current_station].show_station
+  #  puts $trains_at_stations
+    $trains_at_stations[eee.to_sym].push(@train_number)
+
+#наверное должен быть хэш {station1: [t1,t2], station2: [t3,t4],...}
   end
 
   def next_station
@@ -57,7 +61,7 @@ class Train
     route.stations[@current_station-1] if @current_station >= 1
   end
 
-  def self.all_trains
+  def self.show_trains
     @@trains
   end
 
@@ -92,41 +96,65 @@ end
 class Station
   #attr_accessor :current_station
   $trains_at_stations = {}
+  #{station: [t1,t2,...]}
 
   def initialize(station)
     @station = station
     $trains_at_stations[@station] = []
+#    @@all_routes[@first_station] = []
+#    @@all_routes[@last_station] = []
   end
 
   def self.show_stations
     $trains_at_stations
   end
+
+  def go_train(train)
+    @train = train
+    $trains_at_stations.delete(@train)
+  end
+
+  def show_station
+    @station
+  end
+
 end
 
 class Route
 
   attr_reader :stations
-  @@all_routes = []
+  @@all_routes = {}
 
-  def initialize(fs , ls)
-    @fs = fs
-    @ls = ls
-    @stations = [@fs,@ls]
-    @@all_routes.push(@stations)
+  def initialize(first_station , last_station)
+    @first_station = first_station
+    @last_station = last_station
+    @stations = [@first_station,@last_station]
+    @@all_routes[@stations] = []
   end
 
-  def add_station(st)
-    @st = st
-    @stations.insert(@stations.size-1,@st)
+
+  def add_station(station)
+    @station = station
+    @stations.insert(@stations.size-1,@station)
   end
 
-  def del_station(st)
-    @st = st
-    @stations.delete(@st)
+  def del_station(station)
+    @station = station
+    @stations.delete(@station)
   end
 
-  def self.all_routes
+  def self.show_routes
     @@all_routes
   end
 
 end
+
+#s1 = Station.new("nsk")
+#s2 = Station.new("msk")
+#s3 = Station.new("spb")
+
+#r1 = Route.new(s1,s2)
+#r2 = Route.new(s2,s3)
+
+#t1 = Train.new(101,1,5)
+#t2 = Train.new(201,2,7)
