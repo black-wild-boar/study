f3d6a022e7abd3e1c4f2dd54518adb97588c60bf
Mihail <mihailkovalevskiy@gmail.com>
2016-12-20 00:42:29 +0700

    Lesson 5. Simple Tasks.fixed.

--- thinknetica-tasks/lessons/lesson5/modules/company.rb
+++ thinknetica-tasks/lessons/lesson5/modules/company.rb
@@ -9,7 +9,9 @@ module Manufacturer
     end
 #чтобы экземпляры класса не могли напрямую менять компанию, только через методы модуля
   protected
-    attr_reader :company
-  private
-    attr_writer :company
+  attr_accessor :company
+  #protected
+   # attr_reader :company
+  #private
+  #  attr_writer :company
 end
